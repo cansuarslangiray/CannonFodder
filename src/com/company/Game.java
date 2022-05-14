cf7acd9ef68332c223af9c964691ff98940607a5
cansuarslangiray <cansuarslangiray8@gmail.com>
2022-05-14 22:35:15 +0300

    try

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -10,10 +10,9 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome the game!!!");
         System.out.println("Before to start game, you have to know something....");
-        System.out.println("Firstly, When you start the game, you will have one wish. With this wish, you will get the character you will play.");
+        System.out.println("Firstly, When you start the game, you will have characters with 3 different abilities , and have one wish. With this wish, you will get the character or items that will help you during the game, such as weapons..");
         System.out.println("Good luck!!!");
         System.out.println("You can further develop the character you get by wishing, or you can create new characters by wishing again with the Primogen you earn each time you fight.");
-        System.out.println("But remember, there is no such thing as a character will come every time you make a wish, except for the beginning. You can get items that will help you during the game, such as weapons.");
         System.out.println("the adventure rang and your character will start from the lowest level and each time you fight and get stronger the level of the adventure rang will change.");
         System.out.println("As the level of the adventure rang changes, the difficulty level of the enemies you encounter will increase.");
         System.out.println("Secondly, If you are not healthy enough, you can buy food with the coins you earned during the fight, or you can make your own with the materials you earned during the fight.");
